a082722c6635b1aa9fcc9b01c2599e4445aafb7a
morgandevmh <morgandevmh@users.noreply.github.com>
2026-04-13 16:12:52 +0200

    chore: configuration de msw avec handler de test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function startApp() {
+  // active msw en mode dev
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser')
+    await worker.start()
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+startApp()
